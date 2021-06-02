ab8d0c1e34e396f9894a19b615e62f151463c657
CreateValueFor <2019143077@yonsei.ac.kr>
2021-06-02 10:41:03 +0900

    0602final

--- weather.js
+++ weather.js
@@ -1,5 +1,17 @@
+const weather = document.querySelector(".js-weather");
 const API_KEY = "2df5352bd99f09dbc1de0b7a1365efcf";
 const COORDS = 'coords'
+
+function getWeather(lat, lng){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    ).then(function(response){
+    return response.json()
+    }).then(function(json){
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`
+    })
+}
 
 function saveCoords(coordsObj){
     localStorage.setItem(COORDS,JSON.stringify(coordsObj));
@@ -13,6 +25,7 @@ function handleGeoSuccess(position){
         longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude)
 }
 
 function handleGeoError(){
@@ -28,7 +41,8 @@ function loadCoords(){
     if(loadedCords ===null){
         askForCoords();
     } else{
-        //getWeather
+        const parseCoords = JSON.parse(loadedCords)
+        getWeather(parseCoords.latitude, parseCoords.longitude);
     }
 }
 
